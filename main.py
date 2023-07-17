b98be23ec0469c9eb7118e673a876ab292b43623
gabrielbiscaia <gabrielbiscaia23@hotmail.com>
2023-07-17 19:44:53 -0300

    código funcionando

--- main.py
+++ main.py
@@ -3,37 +3,38 @@ import subprocess
 import matplotlib.pyplot as plt
 
 
-
 def compilar_e_rodar(arquivo_c, algoritmo, tamanho, qtdExecucoes, qtdThreads):
-    #Compila
+    # Compila
     comandoCompilacao = ['gcc', arquivo_c, '-o', 'executavel']
-    processoCompilacao = subprocess.run(comandoCompilacao, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    
-    #Verif erros de compilação
+    processoCompilacao = subprocess.run(
+        comandoCompilacao, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    # Verif erros de compilação
     if processoCompilacao.returncode != 0:
         print('Erro de compilação:')
         print(processoCompilacao.stderr.decode('utf-8'))
         return
-    
-    if algoritmo == "1":
-        algoritmo="-s"
-    elif algoritmo =="2":
-        algoritmo ="-p"
 
-    mediaTempoExecucao=0.0
+    if algoritmo == "1":
+        algoritmo = "-s"
+    elif algoritmo == "2":
+        algoritmo = "-p"
+
+    mediaTempoExecucao = 0.0
     temposExec = []
     for i in range(qtdExecucoes):
-            comandoExecucao=['./executavel', algoritmo, tamanho, qtdThreads]
-            #Executa o programa
-            processoExecucao = subprocess.run(comandoExecucao, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            
-            #obtem retorno
-            retorno = processoExecucao.stdout.decode('utf-8')
-            mediaTempoExecucao += float(retorno)
-            temposExec.append(float(retorno))
-            
+        comandoExecucao = ['./executavel', algoritmo, tamanho, qtdThreads]
+        # Executa o programa
+        processoExecucao = subprocess.run(
+            comandoExecucao, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        # obtem retorno
+        retorno = processoExecucao.stdout.decode('utf-8')
+        mediaTempoExecucao += float(retorno)
+        temposExec.append(float(retorno))
+
     mediaTempoExecucao /= qtdExecucoes
-    print("Media do tempo de execução: "+mediaTempoExecucao+"s")
+    print("Media do tempo de execução: ", mediaTempoExecucao, "s")
 
     return mediaTempoExecucao
 
@@ -48,64 +49,63 @@ def compilar_e_rodar(arquivo_c, algoritmo, tamanho, qtdExecucoes, qtdThreads):
     # plt.title('Gráfico de exemplo')
     # plt.show()
 
-#arquivo C
+
+# arquivo C
 arquivo = 'principal.c'
 
 qtdThreads = input("Digite a quantidade de threads do seu computador: ")
-choice= input("Deseja (1) ver o grafico de speedup ou (2) rodar individualmente um algoritmo?")
+choice = input(
+    "Deseja (1) ver o grafico de speedup ou (2) rodar individualmente um algoritmo?\n")
 if choice == "1":
-    threads = [1,2,4,8]
-    mediaParaleloTam1=[]
+    threads = [1, 2, 4, 8]
+    mediaParaleloTam1 = []
 
-    mediaSequencialTam1=compilar_e_rodar(arquivo, "1", "TAM_1", 1, 1)
+    mediaSequencialTam1 = compilar_e_rodar(arquivo, "1", "TAM_1", 1, "1")
 
-    mediaParaleloTam1.append(compilar_e_rodar(arquivo, "2", "TAM_1", 1, 1))
+    mediaParaleloTam1.append(compilar_e_rodar(arquivo, "2", "TAM_1", 1, "1"))
 
-    mediaParaleloTam1.append(compilar_e_rodar(arquivo, "2", "TAM_1", 1, 2))
+    mediaParaleloTam1.append(compilar_e_rodar(arquivo, "2", "TAM_1", 1, "2"))
 
-    mediaParaleloTam1.append(compilar_e_rodar(arquivo, "2", "TAM_1", 1, 4))
+    mediaParaleloTam1.append(compilar_e_rodar(arquivo, "2", "TAM_1", 1, "4"))
 
-    mediaParaleloTam1.append(compilar_e_rodar(arquivo, "2", "TAM_1", 1, 8))
+    mediaParaleloTam1.append(compilar_e_rodar(arquivo, "2", "TAM_1", 1, "8"))
 
-    speedup = [mediaSequencialTam1[0] / mediaParaleloTam1[i] for i in range(len(mediaParaleloTam1))]
+    speedup = [mediaSequencialTam1 / mediaParaleloTam1[i]
+               for i in range(len(mediaParaleloTam1))]
 
     print(speedup)
 
-
     # Plotando o gráfico
-    plt.plot(threads, speedup, 'bo-')  # 'bo-' representa pontos azuis interconectados (blue circles)
+    # 'bo-' representa pontos azuis interconectados (blue circles)
+    plt.plot(threads, speedup, 'bo-')
     plt.xlabel('Número de Threads')
     plt.ylabel('Speedup')
     plt.title('Gráfico de Speedup')
     plt.grid(True)
     plt.show()
 
-
-
     # sequencialTam1=compilar_e_rodar(arquivo, "1", "TAM_2", 3, 1)
 
     # pareleloTam1=compilar_e_rodar(arquivo, "2", "TAM_2", 3, 2)
 
     # pareleloTam2=compilar_e_rodar(arquivo, "2", "TAM_2", 3, 4)
-    
+
     # pareleloTam3=compilar_e_rodar(arquivo, "2", "TAM_2", 3, 8)
-
-
-
 
     # sequencialTam1=compilar_e_rodar(arquivo, "1", "TAM_3", 3, 1)
 
     # pareleloTam1=compilar_e_rodar(arquivo, "2", "TAM_3", 3, 2)
 
     # pareleloTam2=compilar_e_rodar(arquivo, "2", "TAM_3", 3, 4)
-    
+
     # pareleloTam3=compilar_e_rodar(arquivo, "2", "TAM_3", 3, 8)
-elif choice =="2":
-    algoritmo = input("Escolha o algortimo para ser utilizado:\n(1) para Sequencial\n(2) para Paralelo\n")
-    tamanho= input("Qual tamanho da matriz?\n(1) Tamanho = 1000\n(2) Tamanho  = 2000\n(3) Tamanho = 4000\n")
+elif choice == "2":
+    algoritmo = input(
+        "Escolha o algortimo para ser utilizado:\n(1) para Sequencial\n(2) para Paralelo\n")
+    tamanho = input(
+        "Qual tamanho da matriz?\n(1) Tamanho = 1000\n(2) Tamanho  = 2000\n(3) Tamanho = 4000\n")
     qtdExecucoes = input("Deseja rodar o codigo quantas vezes?\n")
-    qtdExecucoes= int(qtdExecucoes)
-     #chama a função para compilar e executar o código C
-    compilar_e_rodar(arquivo, algoritmo, "TAM_"+tamanho,qtdExecucoes, qtdThreads)   
-
-
+    qtdExecucoes = int(qtdExecucoes)
+    # chama a função para compilar e executar o código C
+    compilar_e_rodar(arquivo, algoritmo, "TAM_" +
+                     tamanho, qtdExecucoes, qtdThreads)
--- principal.c
+++ principal.c
@@ -1,64 +1,74 @@
-//importação das bilbiotecas necessárias
+// importação das bilbiotecas necessárias
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
 #include <string.h>
 #include <time.h>
-//importação dos outros arquivos
+// importação dos outros arquivos
 #include "util.c"
 #include "sequencial.c"
 #include "paralelo.c"
-//Quantidade de threads de cada aluno
+// Quantidade de threads de cada aluno
 #define THREADS_ARTHUR 2
 #define THREADS_GABRIEL 8
 #define THREADS_PEDRO 4
-//Tamanho das matrizes
+// Tamanho das matrizes
 #define TAM_1 1000
 #define TAM_2 2000
 #define TAM_3 4000
-//Declaração Matrizes
-float ** matriz1;
-float ** matriz2;
-float ** matrizResultante;
+// Declaração Matrizes
+float **matriz1;
+float **matriz2;
+float **matrizResultante;
 int tamanho;
 int qtdThreads;
-//variaveis para calcular tempo
+// variaveis para calcular tempo
 time_t fim, inicio;
 double tempoFinal;
 
 pthread_mutex_t mutex;
 
-double main(int argc, char* argv[]){
-    
-    
-    tempoFinal=0.0;
-    if(argc==4){
-        if(strcmp(argv[3],"8")){
-            qtdThreads=THREADS_GABRIEL;
+double main(int argc, char *argv[])
+{
+
+    tempoFinal = 0.0;
+    if (argc == 4)
+    {
+        if (strcmp(argv[3], "8"))
+        {
+            qtdThreads = THREADS_GABRIEL;
         }
-        else if(strcmp(argv[3],"4")){
-            qtdThreads=THREADS_PEDRO;
+        else if (strcmp(argv[3], "4"))
+        {
+            qtdThreads = THREADS_PEDRO;
         }
-        else if(strcmp(argv[3],"2")){
-            qtdThreads=THREADS_ARTHUR;
+        else if (strcmp(argv[3], "2"))
+        {
+            qtdThreads = THREADS_ARTHUR;
         }
-        else if(strcmp(argv[3],"1")){
-            qtdThreads=1;
+        else if (strcmp(argv[3], "1"))
+        {
+            qtdThreads = 1;
         }
-                    
-        if (strcmp(argv[2], "TAM_1") == 0) {
+
+        if (strcmp(argv[2], "TAM_1") == 0)
+        {
             tamanho = TAM_1;
-        } else if (strcmp(argv[2], "TAM_2") == 0) {
+        }
+        else if (strcmp(argv[2], "TAM_2") == 0)
+        {
             tamanho = TAM_2;
-        } else if (strcmp(argv[2], "TAM_3") == 0) {
+        }
+        else if (strcmp(argv[2], "TAM_3") == 0)
+        {
             tamanho = TAM_3;
-        } else {
+        }
+        else
+        {
             tamanho = 0;
         }
 
-
-        
-        //Inicia a semente do gerador de numeros aleatórios com o tempo atual.
+        // Inicia a semente do gerador de numeros aleatórios com o tempo atual.
         srand(time(0));
 
         matriz1 = geraMatriz();
@@ -67,17 +77,19 @@ double main(int argc, char* argv[]){
         povoarMatriz(matriz2);
         matrizResultante = geraMatriz();
 
-        if(strcmp(argv[1], "-s")== 0){
-            //Marca o tempo inicial
+        if (strcmp(argv[1], "-s") == 0)
+        {
+            // Marca o tempo inicial
             inicio = time(NULL);
 
-            multiplicaSequencial(); 
+            multiplicaSequencial();
 
             // Marca o tempo final
-            fim = time(NULL);  
+            fim = time(NULL);
         }
-        else if(strcmp(argv[1], "-p")== 0){
-            //Marca o tempo inicial
+        else if (strcmp(argv[1], "-p") == 0)
+        {
+            // Marca o tempo inicial
             inicio = time(NULL);
 
             pthread_t threads[qtdThreads];
@@ -85,28 +97,29 @@ double main(int argc, char* argv[]){
 
             pthread_mutex_init(&mutex, NULL);
 
-            for(int i=0; i<qtdThreads; i++){
-                idsThreads[i]=i;
+            for (int i = 0; i < qtdThreads; i++)
+            {
+                idsThreads[i] = i;
                 pthread_create(&threads[i], NULL, multiplicaParalelo, &idsThreads[i]);
             }
 
-            for(int i=0; i<qtdThreads; i++){
-                pthread_join(threads[i],NULL);
+            for (int i = 0; i < qtdThreads; i++)
+            {
+                pthread_join(threads[i], NULL);
             }
-  
 
             pthread_mutex_destroy(&mutex);
-            //imprimirMatriz(tamanho, tamanho);
-            // Marca o tempo final
+            // imprimirMatriz(tamanho, tamanho);
+            //  Marca o tempo final
             fim = time(NULL);
         }
-        
+
         // Calcula o tempo de CPU utilizado em segundos
- 
+
         liberarMemoria();
-        tempoFinal = fim-inicio;
+        tempoFinal = fim - inicio;
     }
 
+    printf("%lf", tempoFinal);
     return tempoFinal;
-
 }
