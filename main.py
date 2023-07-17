005f07cd049a1774826413f762f57554a38d75d7
Pedro Zafalon <71289265+ranofrolds@users.noreply.github.com>
2023-07-17 18:39:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def compilar_e_rodar(arquivo_c, algoritmo, tamanho, qtdExecucoes):
             mediaTempoExecucao += float(retorno)
             
     mediaTempoExecucao /= qtdExecucoes
-    print("Media do tempo de execução: "+mediaTempoExecucao+"s")
+    print("Media do tempo de execução: ",mediaTempoExecucao,"s")
 
 #arquivo C
 arquivo = 'principal.c'
