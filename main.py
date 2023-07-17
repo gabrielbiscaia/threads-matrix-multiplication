c04fae031b37b82cce693935a3798726287c5941
ranofrolds <pedroh.rz@hotmail.com>
2023-07-17 19:51:39 -0300

    plot grafico

--- main.py
+++ main.py
@@ -60,45 +60,96 @@ if choice == "1":
     threads = [1, 2, 4, 8]
     mediaParaleloTam1 = []
 
-    mediaSequencialTam1 = compilar_e_rodar(arquivo, "1", "TAM_1", 1, "1")
 
-    mediaParaleloTam1.append(compilar_e_rodar(arquivo, "2", "TAM_1", 1, "1"))
+    tempoSequencial1000 = compilar_e_rodar(arquivo, "1", "TAM_1", 1, "1")
+    
+    tempoParalelo1000 = compilar_e_rodar(arquivo, "2", "TAM_1", 1, "1")
+    tempoParalelo1000 = compilar_e_rodar(arquivo, "2", "TAM_1", 1, "2")
+    tempoParalelo1000 = compilar_e_rodar(arquivo, "2", "TAM_1", 1, "4")
+    tempoParalelo1000 = compilar_e_rodar(arquivo, "2", "TAM_1", 1, "8")
 
-    mediaParaleloTam1.append(compilar_e_rodar(arquivo, "2", "TAM_1", 1, "2"))
+    speedup1000= [tempoSequencial1000/tempoParalelo1000[i] for i in range(len(threads))]
 
-    mediaParaleloTam1.append(compilar_e_rodar(arquivo, "2", "TAM_1", 1, "4"))
+    print(speedup1000)
 
-    mediaParaleloTam1.append(compilar_e_rodar(arquivo, "2", "TAM_1", 1, "8"))
+    tempoSequencial2000 = compilar_e_rodar(arquivo, "1", "TAM_1", 1, "1")
+    
+    tempoParalelo2000 = compilar_e_rodar(arquivo, "2", "TAM_1", 1, "1")
+    tempoParalelo2000 = compilar_e_rodar(arquivo, "2", "TAM_1", 1, "2")
+    tempoParalelo2000 = compilar_e_rodar(arquivo, "2", "TAM_1", 1, "4")
+    tempoParalelo2000 = compilar_e_rodar(arquivo, "2", "TAM_1", 1, "8")
 
-    speedup = [mediaSequencialTam1 / mediaParaleloTam1[i]
-               for i in range(len(mediaParaleloTam1))]
+    speedup2000= [tempoSequencial2000/tempoParalelo2000[i] for i in range(len(threads))]
 
-    print(speedup)
+    print(speedup2000)
 
-    # Plotando o gráfico
-    # 'bo-' representa pontos azuis interconectados (blue circles)
-    plt.plot(threads, speedup, 'bo-')
-    plt.xlabel('Número de Threads')
-    plt.ylabel('Speedup')
-    plt.title('Gráfico de Speedup')
-    plt.grid(True)
+    tempoSequencial4000 = compilar_e_rodar(arquivo, "1", "TAM_1", 1, "1")
+    
+    tempoParalelo4000 = compilar_e_rodar(arquivo, "2", "TAM_1", 1, "1")
+    tempoParalelo4000 = compilar_e_rodar(arquivo, "2", "TAM_1", 1, "2")
+    tempoParalelo4000 = compilar_e_rodar(arquivo, "2", "TAM_1", 1, "4")
+    tempoParalelo4000 = compilar_e_rodar(arquivo, "2", "TAM_1", 1, "8")
+
+    speedup4000= [tempoSequencial4000/tempoParalelo4000[i] for i in range(len(threads))]
+
+    print(speedup4000)
+
+
+
+    fig, (ax1, ax2, ax3) = plt.subplots(3, sharex=True)
+
+
+
+
+
+    linear = [i for i in threads]
+
+    # cores: 
+    # chata->vermelho
+    # molina->amarelo
+    # badas->rosa
+    ax1.plot(threads, speedup1000, c='red', label="SpeedUp Paralelo PC-1")
+
+
+    ax2.plot(threads, speedup2000, c='red', label="SpeedUp Paralelo PC-1")
+
+
+    ax3.plot(threads, speedup4000, c='red', label="SpeedUp Paralelo PC-1")
+
+
+
+    ax1.plot(threads, linear, c='green', label="Crescimento Linear")
+    ax1.grid(True)
+
+    ax2.plot(threads, linear, c='green', label="Crescimento Linear")
+    ax2.grid(True)
+
+    ax3.plot(threads, linear, c='green', label="Crescimento Linear")
+    ax3.grid(True)
+
+
+    #ax1.set_xlabel("Threads")
+    #ax1.set_ylabel("SpeedUp")
+    ax1.set_title("Matrix: 1000x1000")
+    # plt.suptitle("Tamanho da matrix: 1000x1000")
+
+    #ax2.set_xlabel("Threads")
+    ax2.set_ylabel("SpeedUp")
+    ax2.set_title("Matrix: 2000x2000")
+
+
+    ax3.set_xlabel("Threads")
+    #ax3.set_ylabel("SpeedUp")
+    ax3.set_title("Matrix: 4000x4000")
+    fig.tight_layout()
+
+
+    # plt.xticks([i+1 for i in range(12)])
+    # plt.yticks([i+1 for i in range(12)])
+
+    # plt.legend()
     plt.show()
 
-    # sequencialTam1=compilar_e_rodar(arquivo, "1", "TAM_2", 3, 1)
-
-    # pareleloTam1=compilar_e_rodar(arquivo, "2", "TAM_2", 3, 2)
-
-    # pareleloTam2=compilar_e_rodar(arquivo, "2", "TAM_2", 3, 4)
-
-    # pareleloTam3=compilar_e_rodar(arquivo, "2", "TAM_2", 3, 8)
-
-    # sequencialTam1=compilar_e_rodar(arquivo, "1", "TAM_3", 3, 1)
-
-    # pareleloTam1=compilar_e_rodar(arquivo, "2", "TAM_3", 3, 2)
-
-    # pareleloTam2=compilar_e_rodar(arquivo, "2", "TAM_3", 3, 4)
-
-    # pareleloTam3=compilar_e_rodar(arquivo, "2", "TAM_3", 3, 8)
 elif choice == "2":
     algoritmo = input(
         "Escolha o algortimo para ser utilizado:\n(1) para Sequencial\n(2) para Paralelo\n")
